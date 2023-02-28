cb9848d0151138d675ba7f14d47fb0b3254d0757
Nguyễn Khánh Duy <duykotlin@gmail.com>
2023-02-28 16:59:29 +0700

    add libraries and jsconfig

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
